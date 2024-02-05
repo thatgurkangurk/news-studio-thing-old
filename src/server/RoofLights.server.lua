c6e9a97feaf2a5be4f9d9099e4e9db60b7fa2ea2
Gurkan <hello@gurkz.me>
2024-02-05 16:52:06 +0100

    countdown!!!!

--- src/server/RoofLights.server.lua
+++ src/server/RoofLights.server.lua
@@ -5,59 +5,17 @@ local Brightness = ServerStorage.RoofLights.Brightness
 local Colour = ServerStorage.RoofLights.Colour
 local RoofLightControls = game.Workspace.Studio.RoofLightControls
 local RoofLights: { Part } = CollectionService:GetTagged("RoofLight")
-local Utils = require(ServerStorage.Utils)
+local RoofLightManager = require(ServerStorage.RoofLightManager)
 
 RoofLightControls.ToggleLight.Button.ProximityPrompt.Triggered:Connect(function()
-	if Brightness.Value == 4 then
-		Brightness.Value = 0
-		Transparency.Value = 1
-	else
-		Brightness.Value = 4
-		Transparency.Value = 0
-	end
+	RoofLightManager.Toggle()
 end)
 
 RoofLightControls.FadeLightToggle.Button.ProximityPrompt.Triggered:Connect(function()
 	if Brightness.Value == 4 then
-		for _, light in ipairs(RoofLights) do
-			local surfaceLight = light:FindFirstChildWhichIsA("SurfaceLight")
-			local transparencyTween = Utils.Tween(light, 2, {
-				Transparency = 1,
-			})
-			local brightnessTween = Utils.Tween(surfaceLight, 2, {
-				Brightness = 0,
-			})
-
-			transparencyTween:Play()
-			brightnessTween:Play()
-
-			transparencyTween.Completed:Connect(function()
-				Transparency.Value = 1
-			end)
-			brightnessTween.Completed:Connect(function()
-				Brightness.Value = 0
-			end)
-		end
+		RoofLightManager.FadeOut()
 	else
-		for _, light in ipairs(RoofLights) do
-			local surfaceLight = light:FindFirstChildWhichIsA("SurfaceLight")
-			local transparencyTween = Utils.Tween(light, 2, {
-				Transparency = 0,
-			})
-			local brightnessTween = Utils.Tween(surfaceLight, 2, {
-				Brightness = 4,
-			})
-
-			transparencyTween:Play()
-			brightnessTween:Play()
-
-			transparencyTween.Completed:Connect(function()
-				Transparency.Value = 0
-			end)
-			brightnessTween.Completed:Connect(function()
-				Brightness.Value = 4
-			end)
-		end
+		RoofLightManager.FadeIn()
 	end
 end)
 
--- src/serverStorage/LightManager.lua
+++ src/serverStorage/LightManager.lua
@@ -1,69 +1,75 @@
 local CollectionService = game:GetService("CollectionService")
 local ServerStorage = game:GetService("ServerStorage")
 local Utils = require(ServerStorage.Utils)
+local IsSpotlightOn = ServerStorage.IsSpotlightOn
 
 local LightManager = {}
 
 LightManager.isTweenFinished = true
 
 function LightManager.ToggleAllLights()
-	for _, part: SpotLight in CollectionService:GetTagged("Spotlight") do
-		if part.Enabled then
-			part.Enabled = false
-			part.Parent.Transparency = 1
-		else
-			part.Enabled = true
-			part.Parent.Transparency = 0
-		end
+	IsSpotlightOn.Value = not IsSpotlightOn.Value
+end
+
+function LightManager.FadeIn()
+	for _, light: SpotLight in CollectionService:GetTagged("Spotlight") do
+		local lampPart: Part = light.Parent
+		light.Enabled = true
+		local endBrightness = ServerStorage.SpotlightBrightness.Value
+
+		local lightTween = Utils.Tween(light, 2, {
+			Brightness = endBrightness,
+		})
+
+		lightTween:Play()
+
+		local lampTween = Utils.Tween(lampPart, 2, {
+			Transparency = 0,
+		})
+
+		lampTween:Play()
+
+		LightManager.isTweenFinished = false
+
+		lightTween.Completed:Connect(function()
+			IsSpotlightOn.Value = true
+			LightManager.isTweenFinished = true
+		end)
+	end
+end
+
+function LightManager.FadeOut()
+	for _, light: SpotLight in CollectionService:GetTagged("Spotlight") do
+		local lampPart: Part = light.Parent
+		local endBrightness = 0
+
+		local lightTween = Utils.Tween(light, 2, {
+			Brightness = endBrightness,
+		})
+
+		lightTween:Play()
+
+		local lampTween = Utils.Tween(lampPart, 2, {
+			Transparency = 1,
+		})
+
+		lampTween:Play()
+
+		LightManager.isTweenFinished = false
+
+		lightTween.Completed:Connect(function()
+			LightManager.isTweenFinished = true
+			light.Enabled = false
+			IsSpotlightOn.Value = false
+		end)
 	end
 end
 
 function LightManager.FadeAllLights()
-	for _, light: SpotLight in CollectionService:GetTagged("Spotlight") do
-		local lampPart: Part = light.Parent
-		if not light.Enabled then
-			light.Enabled = true
-			local endBrightness = ServerStorage.SpotlightBrightness.Value
-
-			local lightTween = Utils.Tween(light, 2, {
-				Brightness = endBrightness,
-			})
-
-			lightTween:Play()
-
-			local lampTween = Utils.Tween(lampPart, 2, {
-				Transparency = 0,
-			})
-
-			lampTween:Play()
-
-			LightManager.isTweenFinished = false
-
-			lightTween.Completed:Connect(function()
-				LightManager.isTweenFinished = true
-			end)
-		else
-			local endBrightness = 0
-
-			local lightTween = Utils.Tween(light, 2, {
-				Brightness = endBrightness,
-			})
-
-			lightTween:Play()
-
-			local lampTween = Utils.Tween(lampPart, 2, {
-				Transparency = 1,
-			})
-
-			lampTween:Play()
-
-			LightManager.isTweenFinished = false
-
-			lightTween.Completed:Connect(function()
-				LightManager.isTweenFinished = true
-				light.Enabled = false
-			end)
-		end
+	if not IsSpotlightOn.Value then
+		LightManager.FadeIn()
+	else
+		LightManager.FadeOut()
 	end
 end
 
